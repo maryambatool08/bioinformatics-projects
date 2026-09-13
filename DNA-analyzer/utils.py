33189d705d5e7eae8eb43eaa6e8516537a4f3e27
maryambatool08 <maryambatool873@gmail.com>
2026-09-14 00:16:19 +0500

    rename files and add validate_sequence

---
+++ DNA-analyzer/utils.py
@@ -0,0 +1,19 @@
+def validate_sequence(seq):
+    seq = "".join(seq.split()).upper()
+
+    if seq == "":
+        raise ValueError("The sequence is empty.")
+
+    valid_bases = set("ATGCN")
+    for position , base in enumerate(seq, start=1):
+        if base not in valid_bases:
+            raise ValueError(f"Invalid base '{base}' at position {position}")
+    return seq
+
+if __name__ == "__main__":
+    for test_seq in ["atgc", "  gc at gc", "ATXGC", ""]:
+        try:
+            print(validate_sequence(test_seq))
+        except ValueError as e:
+            print (f"Error:{e}")
+
